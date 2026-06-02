7f921443b3ce095e51d55680e028a573d1ed2f72
Abhinav <abhinavupadhyay0329@gmail.com>
2026-06-02 16:06:26 +0530

    perf(recursion): inline accumulator calculations to achieve pure tail call form

--- functions/recursion+static_variables/reverse.cpp
+++ functions/recursion+static_variables/reverse.cpp
@@ -1,17 +1,15 @@
 #include <iostream>
 using namespace std;
-int reverse(int n) {
-    static int rev = 0;
-    if (n == 0) {
-        int final_ans = rev;
-        rev = 0; 
-        return final_ans;
-    }
-    rev = (rev * 10) + (n % 10);
-    return reverse(n / 10);
+int reverse(int n,int accumulator_argument=0){
+    if (n == 0) return accumulator_argument;
+    accumulator_argument = (accumulator_argument * 10) + (n % 10);
+    return reverse(n / 10, accumulator_argument);
 }
 
-int main() {
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
     int n;
     cout << "Enter the number : ";
     cin>>n;
